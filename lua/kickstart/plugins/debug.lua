3d6c3dbd50ee662aaa02e67680f598f7a74d9c95
Brian Henderson <brianjhenderson@gmail.com>
2024-08-20 23:16:50 -0400

    remove duplicate and ensure dependancies are installed

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -23,6 +23,7 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    'mfussenegger/nvim-dap-python'
   },
   keys = function(_, keys)
     local dap = require 'dap'
@@ -65,6 +66,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'debugpy'
       },
     }
 
